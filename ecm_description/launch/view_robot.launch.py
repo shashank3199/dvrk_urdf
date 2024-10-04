cbdc8b7eb1c006566125eac839be837009872669
shashank3199 <shashank3199@gmail.com>
2024-10-04 02:56:21 -0400

    Renamed 'urdf.launch.py to view_robot.launch.py

--- ecm_description/launch/view_robot.launch.py
+++ ecm_description/launch/view_robot.launch.py
@@ -1,31 +1,31 @@
 from launch import LaunchDescription
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.descriptions import ParameterValue
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
 
-    file_arg = DeclareLaunchArgument(
-        "file",
-        description="Name of the file"
-    )
-
-    ecm_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("ecm_description"), "urdf", LaunchConfiguration("file")]),
-        ]
+    urdf_arg = DeclareLaunchArgument(
+        "urdf",
+        description="Name of the URDF Xacro file"
     )
 
     ecm_description = {
         "robot_description": ParameterValue(
-            ecm_description_content, value_type=str)
+            Command([
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution(
+                    [FindPackageShare("ecm_description"), "urdf", LaunchConfiguration("urdf")]
+                )
+            ]),
+            value_type=str
+        )
     }
 
     robot_state_publisher_node = Node(
@@ -41,15 +41,21 @@ def generate_launch_description():
         parameters=[ecm_description]
     )
 
+    ecm_rviz_file = PathJoinSubstitution(
+        [FindPackageShare("ecm_description"), "rviz", "ecm_description.rviz"]
+    )
+
+    # RViz2
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
+        arguments=["-d", ecm_rviz_file],
     )
 
     return LaunchDescription([
-        file_arg,
+        urdf_arg,
         robot_state_publisher_node,
         joint_state_publisher_gui_node,
-        rviz_node
+        rviz_node,
     ])
