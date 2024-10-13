c6a67d939ee2d549b956fd84e98adba1d2290b0b
shashank3199 <shashank3199@gmail.com>
2024-10-13 09:03:22 +0000

    Renamed urdf.launch.py to view_robot.launch.py

--- psm_description/launch/view_robot.launch.py
+++ psm_description/launch/view_robot.launch.py
@@ -9,23 +9,30 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
 
-    file_arg = DeclareLaunchArgument(
-        "file",
-        description="Name of the file"
+    urdf_arg = DeclareLaunchArgument(
+        "urdf",
+        description="Name of the URDF Xacro file",
     )
 
-    psm_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("psm_description"), "urdf", LaunchConfiguration("file")]),
-        ]
+    tool_arg = DeclareLaunchArgument(
+        "tool",
+        default_value="sca",
+        description="Name of the tool",
+        choices=["blade", "caudier_blade", "caudier", "sca_blade", "sca", "snake", "P420006", "SF826001"],
     )
 
     psm_description = {
         "robot_description": ParameterValue(
-            psm_description_content, value_type=str)
+            Command([
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution(
+                    [FindPackageShare("psm_description"), "urdf", LaunchConfiguration("urdf")]),
+                " ",
+                "tool:=", LaunchConfiguration("tool")
+            ]),
+            value_type=str
+        )
     }
 
     robot_state_publisher_node = Node(
@@ -41,14 +48,20 @@ def generate_launch_description():
         parameters=[psm_description]
     )
 
+    psm_rviz_file = PathJoinSubstitution(
+        [FindPackageShare("psm_description"), "rviz", "psm_description.rviz"]
+    )
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
+        arguments=["-d", psm_rviz_file],
     )
 
     return LaunchDescription([
-        file_arg,
+        tool_arg,
+        urdf_arg,
         robot_state_publisher_node,
         joint_state_publisher_gui_node,
         rviz_node
