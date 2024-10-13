ddfb85a1e600b7081f158b0fe00534618268eb85
shashank3199 <shashank3199@gmail.com>
2024-10-13 09:54:56 +0000

    Renamed urdf.launch.py to view_robot.launch.py

--- davinci_core_description/launch/view_robot.launch.py
+++ davinci_core_description/launch/view_robot.launch.py
@@ -3,29 +3,20 @@ from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.descriptions import ParameterValue
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
 
-    file_arg = DeclareLaunchArgument(
-        "file",
-        description="Name of the file"
-    )
-
-    daVinciCore_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("davinci_core_description"), "urdf", LaunchConfiguration("file")]),
-        ]
-    )
-
     daVinciCore_description = {
         "robot_description": ParameterValue(
-            daVinciCore_description_content, value_type=str)
+            Command([
+                PathJoinSubstitution([FindExecutable(name="xacro")]),
+                " ",
+                PathJoinSubstitution(
+                    [FindPackageShare("davinci_core_description"), "urdf", "daVinci.urdf.xacro"]),
+            ]),
+            value_type=str
+        )
     }
 
     robot_state_publisher_node = Node(
@@ -41,14 +32,18 @@ def generate_launch_description():
         parameters=[daVinciCore_description]
     )
 
+    davinci_core_rviz_file = PathJoinSubstitution(
+        [FindPackageShare("davinci_core_description"), "rviz", "davinci_core_description.rviz"]
+    )
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
+        arguments=["-d", davinci_core_rviz_file],
     )
 
     return LaunchDescription([
-        file_arg,
         robot_state_publisher_node,
         joint_state_publisher_gui_node,
         rviz_node
